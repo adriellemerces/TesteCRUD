f119040a4419204007d36b23a1f5b29442da0e77
Adrielle Merces <adriellemerces75@gmail.com>
2024-12-09 00:52:11 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace FloressenciaCrud.Model
+{
+   
+        public class Todo
+        {
+              [Key]
+            public int UsuarioId { get; set; }
+            public string nome_usuario { get; set; }
+            public string email { get; set; }
+            public string senha_usuario_hash { get; set; } 
+            //public bool data_nascimento { get; set; }
+            //public bool foto_perfil { get; set; }
+            //public bool data_criacao { get; set; }
+            //public bool data_ultimo_acesso { get; set; }
+    }
+    
+}
---
+++ Model/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FloressenciaCrud.Model
+{
+    public class TodoDb
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+       : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
